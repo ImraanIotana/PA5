0cfb810fd283e0f3577ceb639c686a1ee59dcd67
Imraan Iotana <subpagina_ionisch.0a@icloud.com>
2026-01-21 11:57:44 +0100

    Invoke-Item

--- Update.ps1
+++ Update.ps1
@@ -1,8 +1,13 @@
 $ZipFileOnGithub = 'https://github.com/ImraanIotana/PA5/archive/refs/heads/main.zip'
 $DownloadedFile = 'C:\Users\iotan500\Downloads\PA5.zip'
 
+$PathToRemove = 'C:\Users\iotan500\Downloads\PA5-main'
+if (Test-Path $PathToRemove) {
+    Remove-Item -Path $PathToRemove -Recurse -Force
+}
 
 Invoke-WebRequest $ZipFileOnGithub -OutFile $DownloadedFile
 Expand-Archive -Path $DownloadedFile -DestinationPath C:\Users\iotan500\Downloads -Force
-#Rename-Item .\plinqo-main .\plinqo
-#Remove-Item .\plinqo.zip
+
+Remove-Item -Path $DownloadedFile -Force
+Invoke-Item $PathToRemove
