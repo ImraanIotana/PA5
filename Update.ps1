de1ec5c3c7c1d15a8962820a6a0be9697a5fd0f9
Imraan Iotana <subpagina_ionisch.0a@icloud.com>
2026-01-21 14:16:50 +0100

    Import-FeatureMaintenance

--- Update.ps1
+++ Update.ps1
@@ -1,7 +1,7 @@
 $ZipFileOnGithub = 'https://github.com/ImraanIotana/PA5/archive/refs/heads/main.zip'
 $DownloadedFile = 'C:\Users\iotan500\Downloads\PA5.zip'
 
-$ShortcutToStart = "C:\Users\iotan500\Downloads\PA5-main\Start Packaging Assistant.lnk"
+#$ShortcutToStart = "C:\Users\iotan500\Downloads\PA5-main\Start Packaging Assistant.lnk"
 
 $PathToRemove = 'C:\Users\iotan500\Downloads\PA5-main'
 if (Test-Path $PathToRemove) {
