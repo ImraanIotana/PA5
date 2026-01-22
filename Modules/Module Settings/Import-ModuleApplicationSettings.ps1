2a4e4f8a03cc504e96dc921995f38bc1225fc9f2
Imraan Iotana <subpagina_ionisch.0a@icloud.com>
2026-01-22 04:57:20 +0100

    Import-SubModuleMaintenance

--- Modules/Module Settings/Import-ModuleApplicationSettings.ps1
+++ Modules/Module Settings/Import-ModuleApplicationSettings.ps1
@@ -58,7 +58,6 @@ function Import-ModuleApplicationSettings {
             Import-SubModuleGeneralSettings -ParentTabControl $Global:ModuleApplicationSettingsTabControl
             if (-Not($IsSCCMServer)) { Import-SubModuleAppLockerSettings -ParentTabControl $Global:ModuleApplicationSettingsTabControl }
             Import-SubModuleMaintenance -ParentTabControl $Global:ModuleApplicationSettingsTabControl
-            Import-FeatureUpdates -ParentTabControl $Global:ModuleApplicationSettingsTabControl
         }
     }
     
