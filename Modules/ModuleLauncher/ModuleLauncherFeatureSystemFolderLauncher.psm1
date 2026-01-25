0edb3098d51463f652faf95faebb2b5a805a2d06
Imraan Iotana <subpagina_ionisch.0a@icloud.com>
2026-01-25 09:40:05 +0100

    2c

--- Modules/ModuleLauncher/ModuleLauncherFeatureSystemFolderLauncher.psm1
+++ Modules/ModuleLauncher/ModuleLauncherFeatureSystemFolderLauncher.psm1
@@ -33,7 +33,7 @@ function Import-FeatureSystemFolderLauncher {
         # GroupBox properties
         [PSCustomObject]$GroupBox = @{
             Title           = [System.String]'System Folders'
-            Color           = [System.String]'LightSalmon'
+            Color           = [System.String]'WhiteSmoke'
             NumberOfRows    = [System.Int32]2
         }
 
--- Modules/ModuleLauncher/ModuleLauncherFeatureUserFolderLauncher.psm1
+++ Modules/ModuleLauncher/ModuleLauncherFeatureUserFolderLauncher.psm1
@@ -33,7 +33,7 @@ function Import-FeatureUserFolderLauncher {
         # GroupBox properties
         [PSCustomObject]$GroupBox = @{
             Title           = [System.String]'User Folders'
-            Color           = [System.String]'LightPink'
+            Color           = [System.String]'White'
             NumberOfRows    = [System.Int32]2
             GroupBoxAbove   = $Global:SystemFolderLauncherGroupBox
         }
