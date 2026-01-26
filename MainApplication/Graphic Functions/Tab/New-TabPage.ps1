edec898756025941f3979f2ef41d1dfb2b0c870c
Imraan Iotana <subpagina_ionisch.0a@icloud.com>
2026-01-26 13:23:19 +0100

    078

--- MainApplication/Graphic Functions/Tab/New-TabPage.ps1
+++ MainApplication/Graphic Functions/Tab/New-TabPage.ps1
@@ -3,88 +3,60 @@
 .SYNOPSIS
     This function creates a new TabPage.
 .DESCRIPTION
-    This function is part of the Intune Assistant. It contains references to functions and variables, that may be in other files.
-    External functions: -
-    External variables: $Global:Messages
+    This function is part of the Packaging Assistant. It contains references to functions and variables that are in other files.
 .EXAMPLE
-    New-TabPage -Parent $MyTabControl -Title 'Administration'
+    New-TabPage -ParentTabControl $Global:TabControl -Title 'Administration' -BackGroundColor 'Green'
 .INPUTS
-    This function has 2 mandatory input parameters:
-    Parent(TabControl)  : The TabControl to which this TabPage will be added.
-    Title(String)       : The title of the TabPage.
+    [System.Windows.Forms.TabControl]
+    [System.String]
 .OUTPUTS
-    This function returns a form object of the type [System.Windows.Forms.TabPage].
+    [System.Windows.Forms.TabPage]
 .NOTES
-    Version         : 0.3
-    Author          : Imraan Noormohamed
+    Version         : 5.7.0
+    Author          : Imraan Iotana
     Creation Date   : May 2023
-    Last Updated    : May 2023
+    Last Update     : January 2026
 #>
 ####################################################################################################
 
 function New-TabPage {
     [CmdletBinding()]
     param (
-        # The TabControl to which this TabPage will be added
-        [Parameter(Mandatory=$true)]
-        [System.Windows.Forms.TabControl]
-        $ParentTabControl,
+        [Parameter(Mandatory=$true,HelpMessage='The TabControl to which this TabPage will be added.')]
+        [System.Windows.Forms.TabControl]$ParentTabControl,
 
-        # The title of the TabPage
-        [Parameter(Mandatory=$false)]
-        [System.String]
-        $Title = 'Default Tab Title',
+        [Parameter(Mandatory=$false,HelpMessage='The title of the TabPage.')]
+        [System.String]$Title = 'Default Tab Title',
 
         [Parameter(Mandatory=$false,HelpMessage='The color of the TabPage.')]
-        [System.String]
-        $BackGroundColor
+        [System.String]$BackGroundColor
     )
 
     begin {
-        # Set the main object
-        [PSCustomObject]$Local:MainObject = @{
-            # Function
-            FunctionDetails     = [System.String[]]@($MyInvocation.MyCommand,$PSCmdlet.ParameterSetName,$PSBoundParameters.GetEnumerator())
-            # TabPage object
-            TabPage             = New-Object System.Windows.Forms.TabPage
-            # Input
-            ParentTabControl    = $ParentTabControl
-            TabPageTitle        = $Title
-            BackColor           = $BackGroundColor
-        }
+        ####################################################################################################
+        ### MAIN PROPERTIES ###
+
+        # Output
+        [System.Windows.Forms.TabPage]$OutputObject = New-Object System.Windows.Forms.TabPage
 
         ####################################################################################################
-        ### MAIN FUNCTION METHODS ###
-        
-        # Add the Begin method
-        Add-Member -InputObject $Local:MainObject -MemberType ScriptMethod -Name Begin -Value {
-            # Write the Begin message
-            #Write-Message -FunctionBegin -Details $this.FunctionDetails
-        }
-    
-        # Add the End method
-        Add-Member -InputObject $Local:MainObject -MemberType ScriptMethod -Name End -Value {
-            # Write the End message
-            #Write-Message -FunctionEnd -Details $this.FunctionDetails
-        }
-        
-        # Add the Process method
-        Add-Member -InputObject $Local:MainObject -MemberType ScriptMethod -Name Process -Value {
-            # Add the properties to the TabPage object
-            Write-Verbose 'Creating tabpage...'
-            $this.TabPage.Text = $this.TabPageTitle
-            if ($this.BackColor) { $this.TabPage.BackColor = $this.BackColor }
-            # Add the TabPage the TabControl
-            $this.ParentTabControl.Controls.Add($this.TabPage)
-        }
     } 
     
     process {
-        $Local:MainObject.Process()
+        # Set the Title
+        $OutputObject.Text = $Title
+        # Set the BackGroundColor if provided
+        if ($BackGroundColor) { $OutputObject.BackColor = $BackGroundColor }
+        # Add the TabPage the Parent TabControl
+        $ParentTabControl.Controls.Add($OutputObject)
     }
 
     end {
         # Return the output
-        Return $Local:MainObject.TabPage
+        $OutputObject
     }
 }
+
+### END OF SCRIPT
+####################################################################################################
+
