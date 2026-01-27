f8762c2c2be385eab8bb6b79705b455e344f4efc
Imraan Iotana <subpagina_ionisch.0a@icloud.com>
2026-01-27 04:30:03 +0100

    Invoke-ButtonORG

--- MainApplication/Graphic Functions/Button/Invoke-ButtonORG.ps1
+++ MainApplication/Graphic Functions/Button/Invoke-ButtonORG.ps1
@@ -22,7 +22,7 @@
 #>
 ####################################################################################################
 
-function Invoke-Button {
+function Invoke-ButtonORG {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory=$false,HelpMessage='The Global ApplicationObject containing the Settings.')]
