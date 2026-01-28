286568e4a78defc703bb6fa809942c300fc205a0
Imraan Iotana <subpagina_ionisch.0a@icloud.com>
2026-01-28 10:19:58 +0100

    ModuleGraphicsComboBox

--- MainApplication/Graphic Functions/ComboBox/Update-ComboBox.ps1
+++ MainApplication/Graphic Functions/ComboBox/Update-ComboBox.ps1
@@ -34,55 +34,9 @@ function Update-ComboBox {
     )
     
     begin {
-        # Set the main object
-        [PSCustomObject]$Local:MainObject = @{
-            # Input
-            ComboBoxToupdate        = $ComboBox
-            NewContent              = $NewContent
-            SortContent             = $SortContent
-        }
-
-        ####################################################################################################
-        ### MAIN FUNCTION METHODS ###
-
-        # Add the Process method
-        Add-Member -InputObject $Local:MainObject -MemberType ScriptMethod -Name Process -Value {
-            # Sort the content if requested
-            if ($this.SortContent.IsPresent) { $this.NewContent | Sort-Object } else { $this.NewContent }
-            # Clear the ComboBox items
-            $this.ClearComboBoxItems($this.ComboBoxToupdate)
-            # Update the ComboBox
-            $this.UpdateComboBoxContent($this.ComboBoxToupdate, $this.NewContent)
-        }
-
-        ####################################################################################################
-        ### MAIN PROCESSING METHODS ###
-    
-        # Add the ClearComboBoxItems method
-        Add-Member -InputObject $Local:MainObject -MemberType ScriptMethod -Name ClearComboBoxItems -Value { param([System.Windows.Forms.ComboBox]$ComboBoxToClear)
-            # Clear the ComboBox items
-            $ComboBoxToClear.Items.Clear()
-        }
-
-        # Add the UpdateComboBoxContent method
-        Add-Member -InputObject $Local:MainObject -MemberType ScriptMethod -Name UpdateComboBoxContent -Value { param([System.Windows.Forms.ComboBox]$ComboBoxToUpdate,[System.String[]]$NewContent)
-            try {
-                # Update the ComboBox content
-                foreach ($Item in $NewContent) { $ComboBoxToUpdate.Items.Add($Item) }
-                # Clear the text
-                Invoke-ClipBoard -ClearBox $ComboBoxToUpdate -HideConfirmation
-                # Write the message
-                Write-Host 'UpdateComboBoxContent: The content has been refreshed.' -ForegroundColor DarkGray
-            }
-            catch {
-                Write-FullError
-            }
-        }
     }
     
     process {
-        #$Local:MainObject.Process()
-
         try {
             # SORTING
             # Sort the content
@@ -95,12 +49,11 @@ function Update-ComboBox {
             # UPDATE
             # Update the ComboBox content
             $ContentToUse.ForEach({ $ComboBox.Items.Add($_) | Out-Null })
-
             # Clear the text
             Invoke-ClipBoard -ClearBox $ComboBox -HideConfirmation
             $ComboBox.SelectedIndex = -1
             # Write the message
-            Write-Line 'process: The content has been refreshed.'
+            Write-Line 'The content has been refreshed.'
         }
         catch {
             Write-FullError
