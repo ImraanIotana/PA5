46253b07d54a73f3e85315deb9018134ffc677ce
Imraan Iotana <subpagina_ionisch.0a@icloud.com>
2026-01-28 06:12:08 +0100

    LOCATION

--- MainApplication/Graphic Functions/GroupBox/Invoke-GroupBox.ps1
+++ MainApplication/Graphic Functions/GroupBox/Invoke-GroupBox.ps1
@@ -1,12 +1,9 @@
 ﻿####################################################################################################
 <#
 .SYNOPSIS
-    This function invokes the creation of a new GroupBox, based on the Settings inside the ApplicationObject and the ParentTabPage.
+    This function invokes the creation of a new GroupBox, based on the Settings in the ApplicationObject and the ParentTabPage.
 .DESCRIPTION
-    This function is part of the Packaging Assistant. It contains references to classes, functions or variables, that are in other files.
-    External classes    : -
-    External functions  : Write-Message, New-GroupBox
-    External variables  : $Global:ApplicationObject
+    This function is part of the Packaging Assistant. It contains references to functions and variables that are in other files.
 .EXAMPLE
     Invoke-Groupbox -ParentTabPage $MyTabPage -Title 'Application Input' -NumberOfRows 6 -Color 'Black'
 .EXAMPLE
@@ -30,7 +27,7 @@
 function Invoke-GroupBox {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory=$false,HelpMessage='The Global ApplicationObject containing the Settings.')]
+        [Parameter(Mandatory=$false,HelpMessage='The ApplicationObject containing the Settings.')]
         [PSCustomObject]
         $ApplicationObject = $Global:ApplicationObject,
 
@@ -61,12 +58,21 @@ function Invoke-GroupBox {
 
     begin {
         ####################################################################################################
+        ### MAIN PROPERTIES ###
+
+        # Input
+        [System.Collections.Hashtable]$Settings = $ApplicationObject.Settings
+
+        # Create the GroupBox
+        [System.Windows.Forms.GroupBox]$NewGroupBox = New-Object System.Windows.Forms.GroupBox
+
+        ####################################################################################################
+
+        ####################################################################################################
         ### MAIN OBJECT ###
 
         # Set the main object
         [PSCustomObject]$Local:MainObject = @{
-            # Function
-            FunctionDetails = [System.String[]]@($MyInvocation.MyCommand,$PSCmdlet.ParameterSetName,$PSBoundParameters.GetEnumerator())
             # Input
             Settings        = $ApplicationObject.Settings
             ParentTabPage   = $ParentTabPage
@@ -97,8 +103,6 @@ function Invoke-GroupBox {
 
         # Add the AddGroupBoxLocationToMainObject method
         Add-Member -InputObject $Local:MainObject -MemberType ScriptMethod -Name AddGroupBoxLocationToMainObject -Value { param([System.Collections.Hashtable]$Settings,[System.Windows.Forms.GroupBox]$GroupBoxAbove)
-            # Write the message
-            #Write-Verbose 'Adding the Groupbox location to the Main Object...'
             # Set the TopLeftX of the GroupBox
             [System.Int32]$GroupboxTopLeftX = $Settings.MainTabControl.TopLeftX
             # Set the TopLeftY of the GroupBox
@@ -109,8 +113,6 @@ function Invoke-GroupBox {
     
         # Add the AddGroupBoxSizeToMainObject method
         Add-Member -InputObject $Local:MainObject -MemberType ScriptMethod -Name AddGroupBoxSizeToMainObject -Value { param([System.Collections.Hashtable]$Settings,[System.Int32]$NumberOfRows)
-            # Write the message
-            #Write-Verbose 'Adding the Groupbox size to the Main Object...'
             # Get the Width and Height of the GroupBox from the Global Settings
             [System.Int32]$Width    = if ($this.OnSubTab.IsPresent) { $Settings.GroupBox.SubTabGroupboxWidth } else { $Settings.GroupBox.Width }
             [System.Int32]$Height   = $Settings.GroupBox.HeightTable.($NumberOfRows)
@@ -124,25 +126,28 @@ function Invoke-GroupBox {
         # Add the CreateGroupbox method
         Add-Member -InputObject $Local:MainObject -MemberType ScriptMethod -Name CreateGroupbox -Value { param([System.Windows.Forms.TabPage]$ParentTabPage,[System.Int32[]]$Location,[System.Int32[]]$Size,[System.String]$Title,[System.String]$Color)
             # Create the GroupBox
-            #Write-Verbose 'Creating the new GroupBox and adding it to the Parent TabPage...'
             $this.OutputObject = New-GroupBox -ParentTabPage $ParentTabPage -Location $Location -Size $Size -Title $Title -Color $Color
         }
 
         ####################################################################################################
 
-        # Write the Begin message
-        #Write-Message -FunctionBegin -Details $Local:MainObject.FunctionDetails
     }
     
     process {
-        $Local:MainObject.Process()
+        #$Local:MainObject.Process()
+        
+        # LOCATION
+        # Set the TopLeftX of the GroupBox
+        [System.Int32]$GroupboxTopLeftX = $Settings.MainTabControl.TopLeftX
+        # Set the TopLeftY of the GroupBox
+        [System.Int32]$GroupboxTopLeftY = if ($GroupBoxAbove) { $GroupBoxAbove.Location.Y + $GroupBoxAbove.Height + $Settings.GroupBox.BetweenMargin } else { $Settings.MainTabControl.TopLeftY }
+        # Set the Location
+        [System.Int32[]]$Location = @($GroupboxTopLeftX, $GroupboxTopLeftY)
     }
 
     end {
         # Return the output
         $Local:MainObject.OutputObject
-        # Write the End message
-        #Write-Message -FunctionEnd -Details $Local:MainObject.FunctionDetails
     }
 }
 
