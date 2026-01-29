789470f8a5d0ad4cb0f9345f36bf7d183a0e06e6
Imraan Iotana <subpagina_ionisch.0a@icloud.com>
2026-01-29 11:42:53 +0100

    0210

---
+++ MainApplication/Shared Functions/Reset-BoxToDefault.ps1
@@ -0,0 +1,58 @@
+####################################################################################################
+<#
+.SYNOPSIS
+    This function resets a TextBox/ComboBox to their Default Value.
+.DESCRIPTION
+    This function is part of the Packaging Assistant. It contains references to functions and variables that are in other files.
+.EXAMPLE
+    Reset-BoxToDefault
+.INPUTS
+    [System.Object]
+.OUTPUTS
+    This function returns no stream output.
+.NOTES
+    Version         : 5.7.0.0210
+    Author          : Imraan Iotana
+    Creation Date   : January 2026
+    Last Update     : January 2026
+.COPYRIGHT
+    Copyright (C) Iotana. All rights reserved.
+#>
+####################################################################################################
+
+function Reset-BoxToDefault {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true,HelpMessage='The box that will be reset to their Defualt Value.')]
+        [System.Object]$Box
+    )
+
+    begin {
+        ####################################################################################################
+        ### MAIN PROPERTIES ###
+
+        # Handlers
+        [System.String]$DefaultValue        = $Box.DefaultValue
+        [System.String]$ConfirmationTitle   = 'Reset to default'
+        [System.String]$ConfirmationBody    = "This will reset this field to the default value:`n`n({0})`n`nAre you sure?"
+
+        ####################################################################################################
+    }
+    
+    process {
+        # VALIDATION
+        if ([System.String]::IsNullOrEmpty($DefaultValue) ) { Write-Line "This box has no Default Value. No action has been taken." ; Return }
+
+        # CONFIRMATION
+        if ( -Not( Get-UserConfirmation -Title $ConfirmationTitle -Body ($ConfirmationBody -f $DefaultValue) ) ) { Return }
+
+        # EXECUTION
+        $Box.Text = $Box.DefaultValue
+    }
+    
+    end {
+    }
+}
+
+### END OF SCRIPT
+####################################################################################################
