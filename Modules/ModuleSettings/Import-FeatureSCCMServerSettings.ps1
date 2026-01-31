d78ba78d1802b411205627d9e980e6ceeca7b06a
Imraan Iotana <subpagina_ionisch.0a@icloud.com>
2026-01-31 11:18:08 +0100

    Global:ASSSSiteCodeTextBox

--- Modules/ModuleSettings/Import-FeatureSCCMServerSettings.ps1
+++ Modules/ModuleSettings/Import-FeatureSCCMServerSettings.ps1
@@ -70,7 +70,7 @@ function Import-FeatureSCCMServerSettings {
                 ColumnNumber    = 5
                 Text            = 'Default'
                 #Function        = { & $Global:ASSSSiteCodeTextBox.Tag.Reset }
-                Function        = { $Global:ASSSSiteCodeTextBox.ResetToDefaultValue() }
+                Function        = { & $Global:ASSSSiteCodeTextBox.Tag.ResetToDefaultValue }
             }
         )
 
