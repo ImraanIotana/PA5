baaa34df05234a68a5a6ee99391a0fd7a49b221c
Imraan Iotana <subpagina_ionisch.0a@icloud.com>
2026-01-31 09:13:28 +0100

    0223

--- Modules/ModuleSettings/SubModuleSCCMSettings.psm1
+++ Modules/ModuleSettings/SubModuleSCCMSettings.psm1
@@ -32,39 +32,12 @@ function Import-SubModuleSCCMSettings {
         # Handlers
         [System.String]$TabTitle        = 'SCCM Settings'
         [System.String]$ModuleVersion   = '5.7.0'
-        [System.String]$BackGroundColor = 'YellowGreen'
+        [System.String]$BackGroundColor = 'ForestGreen'
 
         ####################################################################################################
-        ####################################################################################################
-        ### MAIN OBJECT ###
-
-        # Set the main object
-        [PSCustomObject]$Local:MainObject = @{
-            # Input
-            ParentTabControl    = $ParentTabControl
-            # Handlers
-            TabTitle            = [System.String]'SCCM Settings'
-            ModuleVersion       = [System.String]'5.4.7'
-            # Message Handlers
-            ImportMessage       = [System.String]('Importing SubModule {0} {1}')
-        }
-
-        ####################################################################################################
-        ### MAIN FUNCTION METHODS ###
-
-        # Add the Process method
-        Add-Member -InputObject $Local:MainObject -MemberType ScriptMethod -Name Process -Value {
-            # Write the message
-            Write-Host ($this.ImportMessage -f $this.TabTitle,$this.ModuleVersion) -ForegroundColor DarkGray
-            # Create the Module TabPage
-            [System.Windows.Forms.TabPage]$ParentTabPage = $Global:SubModuleSCCMSettingsTabPage = New-TabPage -Parent $this.ParentTabControl -Title $this.TabTitle -BackGroundColor 'YellowGreen'
-            # Import the Features
-            Import-FeatureSCCMServerSettings -ParentTabPage $ParentTabPage
-        }
     }
     
     process {
-        #$Local:MainObject.Process()
         try {
             # Write the message
             Write-Line "Importing SubModule $TabTitle $ModuleVersion"
