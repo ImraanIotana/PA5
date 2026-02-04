6d89b6c22a7050d3e7af3b259ff3f33a3626498b
Imraan Iotana <subpagina_ionisch.0a@icloud.com>
2026-02-04 17:41:26 +0100

    086

--- PackagingAssistant.ps1
+++ PackagingAssistant.ps1
@@ -36,7 +36,7 @@ begin {
     [PSCustomObject]$Global:ApplicationObject = @{
         # Application
         Name                        = [System.String]'Packaging Assistant'
-        Version                     = [System.String]'5.7.1.085'
+        Version                     = [System.String]'5.7.1.086'
         # Folder Handlers
         RootFolder                  = [System.String]$PSScriptRoot
         LogFolder                   = [System.String](Join-Path -Path $ENV:TEMP -ChildPath 'PALogs')
