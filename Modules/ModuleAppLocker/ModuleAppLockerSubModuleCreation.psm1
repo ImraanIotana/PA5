c839ba15768d8048cbf2586112ea8161fb62482d
Imraan Iotana <subpagina_ionisch.0a@icloud.com>
2026-02-14 04:55:47 +0100

    349

--- Modules/ModuleAppLocker/ModuleAppLockerSubModuleCreation.psm1
+++ Modules/ModuleAppLocker/ModuleAppLockerSubModuleCreation.psm1
@@ -84,16 +84,12 @@ function Import-FeatureAppLockerCreation {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory=$true,HelpMessage='The Parent TabPage to which this Feature will be added.')]
-        [System.Windows.Forms.TabPage]
-        $ParentTabPage
+        [System.Windows.Forms.TabPage]$ParentTabPage
     )
 
     begin {
         ####################################################################################################
         ### MAIN PROPERTIES ###
-
-        # Function
-        [System.String[]]$FunctionDetails   = @($MyInvocation.MyCommand,$PSCmdlet.ParameterSetName,$PSBoundParameters.GetEnumerator())
 
         # GroupBox properties
         [PSCustomObject]$GroupBox   = @{
@@ -101,6 +97,9 @@ function Import-FeatureAppLockerCreation {
             Color                   = [System.String]'Cyan'
             NumberOfRows            = [System.Int32]8
         }
+
+        # Set the Button Properties Array
+        [System.Object[][]]$ButtonPropertiesArray  = @( (1,'Browse') , (2,'Copy') , (3,'Paste') , (5,'Open') )
 
         ####################################################################################################
         ### BUTTON PROPERTIES ###
@@ -122,28 +121,28 @@ function Import-FeatureAppLockerCreation {
                 Text            = 'Browse'
                 Image           = 'folders_explorer.png'
                 SizeType        = 'Medium'
-                Function        = { [System.String]$Folder = Select-Item -Folder ; if ($Folder) { $Global:ALCFolderToScanTextBox.Text = $Folder } }
+                Function        = { [System.String]$Folder = Select-Item -Folder ; if ($Folder) { $Script:MAppLock_SCreate_FolderToScan_TextBox.Text = $Folder } }
             }
             @{
                 ColumnNumber    = 2
                 Text            = 'Copy'
                 Image           = 'page_copy.png'
                 SizeType        = 'Medium'
-                Function        = { Invoke-ClipBoard -CopyFromBox $Global:ALCFolderToScanTextBox }
+                Function        = { Invoke-ClipBoard -CopyFromBox $Script:MAppLock_SCreate_FolderToScan_TextBox }
             }
             @{
                 ColumnNumber    = 3
                 Text            = 'Paste'
                 Image           = 'page_paste.png'
                 SizeType        = 'Medium'
-                Function        = { Invoke-ClipBoard -PasteToBox $Global:ALCFolderToScanTextBox }
+                Function        = { Invoke-ClipBoard -PasteToBox $Script:MAppLock_SCreate_FolderToScan_TextBox }
             }
             @{
                 ColumnNumber    = 4
                 Text            = 'Open'
                 Image           = 'folder.png'
                 SizeType        = 'Medium'
-                Function        = { Open-Folder -Path $Global:ALCFolderToScanTextBox.Text }
+                Function        = { Open-Folder -Path $Script:MAppLock_SCreate_FolderToScan_TextBox.Text }
             }
         )
 
@@ -171,35 +170,34 @@ function Import-FeatureAppLockerCreation {
                 DefaultIcon     = 'Shield'
                 SizeType        = 'Large'
                 ToolTip         = 'Create AppLocker Policy Files from the selected folder'
-                Function        = { Invoke-AppLockerFileCreation -ApplicationID $Global:ALCApplicationIDComboBox.Text -FolderToScan $Global:ALCFolderToScanTextBox.Text -ADGroupSID $Global:ALCADGroupSIDTextBox.Text }
+                Function        = { Invoke-AppLockerFileCreation -ApplicationID $Global:ALCApplicationIDComboBox.Text -FolderToScan $Script:MAppLock_SCreate_FolderToScan_TextBox.Text -ADGroupSID $Global:ALCADGroupSIDTextBox.Text }
             }
         )
 
         ####################################################################################################
-
-        # Write the begin message
-        Write-Function -Begin $FunctionDetails
     } 
     
     process {
         # Create the GroupBox
         [System.Windows.Forms.GroupBox]$Global:AppLockerCreationGroupbox = $ParentGroupBox = Invoke-Groupbox -ParentTabPage $ParentTabPage -Title $Groupbox.Title -NumberOfRows $Groupbox.NumberOfRows -Color $Groupbox.Color
+
         # Create the ApplicationID ComboBox
         [System.Windows.Forms.ComboBox]$Global:ALCApplicationIDComboBox = Invoke-ComboBox -ParentGroupBox $ParentGroupBox -RowNumber 1 -SizeType Medium -Type Output -Label 'Select Application:' -ContentArray (Get-DSLApplicationFolder -All -Basenames) -PropertyName 'ALCApplicationIDComboBox'
         Invoke-ButtonLine -ButtonPropertiesArray $ALCApplicationIDComboBoxButtons -ParentGroupBox $ParentGroupBox -RowNumber 1
-        # Create the FolderToScan TextBox
-        [System.Windows.Forms.TextBox]$Global:ALCFolderToScanTextBox = Invoke-TextBox -ParentGroupBox $ParentGroupBox -RowNumber 3 -SizeType Medium -Type Input -Label 'Select Folder to scan:' -PropertyName 'ALIFolder1ToScanTextBox'
+
+        # Create the MAppLock_SCreate_FolderToScan_TextBox
+        [System.Windows.Forms.TextBox]$Script:MAppLock_SCreate_FolderToScan_TextBox = Invoke-TextBox -ParentGroupBox $ParentGroupBox -RowNumber 3 -SizeType Medium -Type Input -Label 'Select Folder to scan:' -PropertyName 'MAppLock_SCreate_FolderToScan_TextBox'
         Invoke-ButtonLine -ButtonPropertiesArray $ALCFolderToScanTextBoxButtons -ParentGroupBox $ParentGroupBox -RowNumber 4
+
         # Create the ALIADGroupSIDTextBox
         [System.Windows.Forms.TextBox]$Global:ALCADGroupSIDTextBox = Invoke-TextBox -ParentGroupBox $ParentGroupBox -RowNumber 6 -SizeType Medium -Type Input -Label 'AD Group SID:' -PropertyName 'ALCADGroupSIDTextBox'
         Invoke-ButtonLine -ButtonPropertiesArray $ALCADGroupSIDTextBoxButtons -ParentGroupBox $ParentGroupBox -RowNumber 7
+
         # Create the action button
         Invoke-ButtonLine -ButtonPropertiesArray $ActionButtons -ParentGroupBox $ParentGroupBox -RowNumber 8
     }
 
     end {
-        # Write the end message
-        Write-Function -End $FunctionDetails
     }
 }
 
